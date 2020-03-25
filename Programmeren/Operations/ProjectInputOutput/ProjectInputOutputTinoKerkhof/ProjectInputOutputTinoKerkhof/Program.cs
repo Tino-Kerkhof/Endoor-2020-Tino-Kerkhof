29e85f2e9443d42adb32216bd88fb948f8b1d0e6
Tino Kerkhof <endoor-2020-024@outlook.com>
2020-03-25 15:07:18 +0100

    Project Input Output

---
+++ Programmeren/Operations/ProjectInputOutput/ProjectInputOutputTinoKerkhof/ProjectInputOutputTinoKerkhof/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ProjectInputOutputTinoKerkhof
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * This program asks the user to give the amount of money he/she has 
+             * and tells him/her how many chocolatebars he can buy.
+             */
+            decimal myMoney;
+            decimal chocolatePrice = 1.50m;
+            decimal numberOfChocolates;
+
+            Console.Write("How much money do you have? (use \",\" as a decimal devider) $");
+            myMoney = decimal.Parse(Console.ReadLine());
+
+            numberOfChocolates = Math.Floor(myMoney / chocolatePrice);
+
+            Console.WriteLine($"You can buy {numberOfChocolates}.");
+        }
+    }
+}
