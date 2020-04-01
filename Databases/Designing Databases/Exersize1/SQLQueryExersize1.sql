8409bdf04a7bd24ae893f340943270e8481f6434
Tino Kerkhof <endoor-2020-024@outlook.com>
2020-04-01 15:11:43 +0200

    programmeren opdrachten en database begin

---
+++ Databases/Designing Databases/Exersize1/SQLQueryExersize1.sql
@@ -0,0 +1,17 @@
+-- create new table to hold rows
+USE DoctorWho
+CREATE TABLE tblProductionCompany(
+	ProductionCompanyId int IDENTITY(1,1) PRIMARY KEY,
+	ProductionCompanyName nvarchar(100),
+	Abbriviation nvarchar(15)
+)
+go
+-- insert 2 new rows
+INSERT INTO tblProductionCompany(
+	ProductionCompanyName,
+	Abbriviation
+) 
+VALUES 
+	('British Broadcasting Assosiation', 'BBC'),
+	('Canadian Broadcasting Assosiation', 'CBC');
+go
